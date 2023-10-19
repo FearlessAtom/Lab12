ac4a5f8653d60d0e4c56a75af88c08583177c2a9
FearlessAtom <atomgotaggressive@gmail.com>
2023-10-19 14:27:25 +0300

    19.10,14:27

---
+++ task1/task1/task1.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main(){
+    int r = 19, sum, something = 0, difference, difference2,first,second;
+    int array[10] = {9, 5, 8, 7, 1, 3, 2, 5, 6, 3};
+    for (int index = 0; index < 9; index++) {
+        sum = array[index] + array[index + 1];
+        difference = r - sum;
+        difference2 = r - something;
+        if (abs(difference) < abs(difference2)) {
+            something = sum;
+            first = array[index];
+            second = array[index + 1];}}
+    printf("The sum is : %d\n", something);
+    printf("The first one is : %d\n",first);
+    printf("The second one is : %d\n",second);
+    return 0;}
---
+++ task2/task2/task2.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+int main(){
+    int sum;
+    int arraya[10],arrayb[10];
+    for(int index = 0;index < 10;index++){
+        printf("The element %d is : ",index + 1);scanf_s("%d",&arraya[index]);}
+    for(int index = 0;index < 10;index++){
+        sum = 0;
+        for(int j = 0;j <= index;j++){
+            sum = sum + arraya[j];}
+        if(index){sum = sum / (index + 1);}
+        arrayb[index] = sum;}
+    printf("array B = {");
+    for(int index = 0;index < 10;index++){
+        printf("%d",arrayb[index]);
+        if(index != 9){printf(" , ");}}
+    printf("}");
+    return 0;}
